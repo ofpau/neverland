cf26fab9f453ba587a3b1d2e69cfda6e05abc799
BugMan42 <guillem812@gmail.com>
2017-11-25 23:41:30 +0000

    Add first http request

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import requests
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/obtainLocations")
+def obtain():
+    r = requests.get('https://api.finnair.com/aws/locations/prod/all')
+    #print(r.text)
+    return r.text
