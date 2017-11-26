9940fbbbc456fffd39ce40f32788429245f9a798
Pau Oliver <pau.oliver@est.fib.upc.edu>
2017-11-26 01:40:44 +0100

    Start series view

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template, request, redirect, session
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('index.html')#, images=images, user=user)
+    #return "Hello World!"
+
+
+@app.route("/series")
+def sesries():
+    series = [{'id':'got', 'name': 'Game of Thrones'}, {'id': 'narcos', 'name': 'Narcos'}]
+    return render_template('series.html', series=series)
